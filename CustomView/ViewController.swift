2b8bbe1a20da5617d81733d0efe96b7db375dc3c
Technotroop <tt@technotroops-mac-mini.local>
2016-01-27 17:56:59 +0545

    Initial Commit

---
+++ CustomView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomView
+//
+//  Created by Anit Shrestha Manadhar on 1/27/16.
+//  Copyright © 2016 Technotroop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
